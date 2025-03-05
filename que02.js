c6524d13645c886fc739bea484200e27fb6004de
AmitRawat01 <amitrawat8774@gmail.com>
2025-03-05 13:45:06 +0530

    que2 updated

--- que02.js
+++ que02.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://user10:user1000@cluster0.gzh1q.mongodb.net/?retryWrites=true&w=majority";
+
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 const calculateAverageAge = async () => {
